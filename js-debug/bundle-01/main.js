cd9770d7b74634fd20d440200e4ff4bbf11fb5e5
Robertarezza <roberta.rezza@gmail.com>
2024-04-16 15:20:16 +0200

    add: es 4 bundle-01/

--- js-debug/bundle-01/main.js
+++ js-debug/bundle-01/main.js
@@ -26,24 +26,28 @@
 
 // console.log(addIfEven(2));//se inserisco 2 mi restituisce 7 se inserisco 5 mi restituisce 5 perchè 5 non è div per 2
 
-// // ESERCIZIO 3 // mancavano i ; dopo lo 0 e il 5 nel ciclo for
-function loopToFive() {
+// // // ESERCIZIO 3 // mancavano i ; dopo lo 0 e il 5 nel ciclo for
+// function loopToFive() {
 
-    for (let i = 0; i < 5; i++) {
-        console.log(i);
-    }
-}
- loopToFive();//al richiamo la funzione stampa in console i num da 0 a 4 come richiesto
-
-// // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
-// function displayEvenNumbers() {
-//     let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
-//     let evenNumbers = [];
-//     for (let i = 0; i < numbers.length - 1; i++;) {
-//         if (numbers % 2 = 0); {
-//             evenNumbers.push(i);
-//         }
-//         return evenNumbers;
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
 //     }
 // }
-// displayEvenNumbers(); // dovrebbe restituire [2,4,6,8]
+//  loopToFive();//al richiamo la funzione stampa in console i num da 0 a 4 come richiesto
+
+// // ESERCIZIO 4 (suggerimento: ci sono 7 errori)
+function displayEvenNumbers() {
+    let numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+    let evenNumbers = [];
+
+    for (let i = 0; i < numbers.length; i++) { //1 non va il ; dopo i++ // 4 non va -1 se no non prenderà in considerazione l'ultimo indice
+
+        if (numbers[i] % 2 === 0) { //2 mancano altri due ==, 3 non va ; dopo la par tonda della condizione,//5 visto che non abbiamo dichiarato l'elemento corrente quin va number[i]
+            evenNumbers.push(numbers[i]);//6 anche qui va pusch(numbers[i])
+          
+        }
+        
+    }
+    return evenNumbers;// 7 il return va fuori dal ciclo for
+}
+console.log(displayEvenNumbers()); // dovrebbe restituire [2,4,6,8]
